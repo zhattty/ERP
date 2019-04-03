b69871f689e050aea0f1b68f07f33cfc679aea5e
wangjinghai <972541876@qq.com>
2019-04-03 23:09:11 +0800

    technology的搜索和查询  by jackey_wang 20190403

--- erp/src/main/java/com/erp/web4j/mapper/TechnologyMapper.java
+++ erp/src/main/java/com/erp/web4j/mapper/TechnologyMapper.java
@@ -1,6 +1,9 @@
 package com.erp.web4j.mapper;
 
 import com.erp.web4j.bean.Technology;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface TechnologyMapper {
     int deleteByPrimaryKey(String technologyId);
@@ -14,4 +17,10 @@ public interface TechnologyMapper {
     int updateByPrimaryKeySelective(Technology record);
 
     int updateByPrimaryKey(Technology record);
+
+    List<Technology> findTechnology(@Param("offset") Integer offset,@Param("rows") Integer rows);
+
+    List<Technology> searchByPrimaryKey(@Param("searchValue") String searchValue,@Param("offset") Integer offset, @Param("rows")Integer rows);
+
+    List<Technology> searchByName(@Param("searchValue") String searchValue,@Param("offset") Integer offset, @Param("rows")Integer rows);
 }
---
+++ erp/src/main/java/com/erp/web4j/service/TechnologyService.java
@@ -0,0 +1,24 @@
+package com.erp.web4j.service;
+
+import com.erp.web4j.bean.Technology;
+import com.erp.web4j.mapper.TechnologyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by wawawa
+ * Date 2019/4/3 Time 19:36
+ */
+public interface TechnologyService {
+
+    /**
+     *查询所有的technology，并且附带搜索功能
+     * 此处未加上分页，后续应该可以操作
+     */
+    List<Technology> findTechnology(Integer page,Integer rows);
+
+    List<Technology> searchByPrimaryKey(String  searchValue,Integer page,Integer rows);
+
+    List<Technology> searchByName(String searchValue, Integer page, Integer rows);
+}
