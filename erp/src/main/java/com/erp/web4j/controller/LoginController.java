967e4c4e29b523aff78a6fced7cd83c52211f40e
ZhangYao <1062577487@qq.com>
2019-04-05 15:35:19 +0800

    Update LoginController.java

--- erp/src/main/java/com/erp/web4j/controller/LoginController.java
+++ erp/src/main/java/com/erp/web4j/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.erp.web4j.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by wawawa
