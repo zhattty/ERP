91e2375905844fde6d5864b9e5e3f07ba1f6e39e
wangjinghai <972541876@qq.com>
2019-04-05 11:30:24 +0800

    111 by 王静海 20190403

--- erp/src/main/java/com/erp/web4j/service/TechnologyService.java
+++ erp/src/main/java/com/erp/web4j/service/TechnologyService.java
@@ -16,9 +16,9 @@ public interface TechnologyService {
      *查询所有的technology，并且附带搜索功能
      * 此处未加上分页，后续应该可以操作
      */
-    List<Technology> findTechnology(Integer page,Integer rows);
+    List<Technology> findTechnology(Integer page, Integer rows);
 
-    List<Technology> searchByPrimaryKey(String  searchValue,Integer page,Integer rows);
+    List<Technology> searchByPrimaryKey(String searchValue, Integer page, Integer rows);
 
     List<Technology> searchByName(String searchValue, Integer page, Integer rows);
 
@@ -26,7 +26,7 @@ public interface TechnologyService {
 
     boolean update(Technology technology);
 
-    boolean deleteBatch(int []ids);
+    boolean deleteBatch(int[] ids);
 
     Technology selectTechnologyById(String technologyName);
 }
