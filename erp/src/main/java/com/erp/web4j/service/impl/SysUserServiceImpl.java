47a0cc513b7bf40cd33d0bc0fcd2cc022e45abbb
wangjinghai <972541876@qq.com>
2019-04-06 09:49:52 +0800

    工序管理 by 王静海 20190403

--- erp/src/main/java/com/erp/web4j/service/impl/SysUserServiceImpl.java
+++ erp/src/main/java/com/erp/web4j/service/impl/SysUserServiceImpl.java
@@ -1,3 +1,4 @@
+
 package com.erp.web4j.service.impl;
 
 import com.erp.web4j.bean.SysUser;
@@ -6,22 +7,27 @@ import com.erp.web4j.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 /**
  * Created by ZhangYao
  * Date 2019/4/4 Time 11:40
  */
+
 @Service
 public class SysUserServiceImpl implements SysUserService {
     @Autowired
     SysUserMapper sysUserMapper;
 
-    /**
+
+/**
      * 通过用户名来查找user，可能返回null
      * @param username
      * @return
      */
+
     @Override
     public SysUser selectSysUserByUsername(String username) {
         return sysUserMapper.selectSysUserByUsername(username);
     }
 }
+
